f890ca6d760a47470097adfc749cb3de92c9409a
brandoll0530 <branollguzman@gmail.com>
2023-04-26 18:59:08 -0600

    avance

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <article className=' text-white bg-[#1A1A1AE5] bg-opacity-70  rounded-xl   pb-10 w-full pt-10 mb-16'>
+        <h1 className=' p-5 font-bold text-[32px] text-center'>
+        Contact me!
+        </h1>
+        <p className=' font-semibold text-center'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        </p>
+        <div className=' flex justify-center'> 
+        <button className='border border-white px-4 py-1 rounded-md mt-6 mx-auto'>
+        Click here
+        </button>
+        </div>
+
+        
+    </article>
+  )
+}
+
+export default Footer
